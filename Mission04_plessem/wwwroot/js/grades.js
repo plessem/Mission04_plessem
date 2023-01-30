7f1437c0cd0cb4e38428daa76d9438858b815cb8
Emma Plessinger <plessinger.em@gmail.com>
2023-01-29 22:32:48 -0700

    Add project files.

---
+++ Mission04_plessem/wwwroot/js/grades.js
@@ -0,0 +1,7 @@
+﻿//function to calculate final grade and letter grade
+//$("#buttonSend").click(function () {
+//    alert("From: " + $("#textfrom").val() +
+//        ", Subject: " + $("#subject").val() +
+//        ", Message: " + $("#message").val());
+
+//})
