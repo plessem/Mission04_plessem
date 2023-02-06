22e4518be05c80c932e8936811a8e4bd368efe86
Emma Plessinger <plessinger.em@gmail.com>
2023-02-05 22:20:52 -0700

    testing creating branch

---
+++ Mission04_plessem/Controllers/BlahController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_plessem.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new IndexOutOfRangeException();
+            return View();
+        }
+    }
+}
